142a6372bcb050c90f36e102c51d00d34e55db23
yatay1408 <70847533+yatay1408@users.noreply.github.com>
2024-07-02 08:04:55 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('BLE Scanner'),
+        ),
+        body: BleScanner(),
+      ),
+    );
+  }
+}
+
+class BleScanner extends StatefulWidget {
+  @override
+  _BleScannerState createState() => _BleScannerState();
+}
+
+class _BleScannerState extends State<BleScanner> {
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  List<BluetoothDevice> devicesList = List<BluetoothDevice>();
+
+  @override
+  void initState() {
+    super.initState();
+    startScan();
+  }
+
+  void startScan() {
+    flutterBlue.startScan(timeout: Duration(seconds: 4));
+    flutterBlue.scanResults.listen((results) {
+      for (ScanResult r in results) {
+        setState(() {
+          devicesList.add(r.device);
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: devicesList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(devicesList[index].name),
+          onTap: () async {
+            await devicesList[index].connect();
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DeviceScreen(device: devicesList[index])),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class DeviceScreen extends StatelessWidget {
+  final BluetoothDevice device;
+
+  DeviceScreen({Key key, this.device}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Device: ${device.name}'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Code to send data goes here
+          },
+          child: Text('Send Data'),
+        ),
+      ),
+    );
+  }
+}
